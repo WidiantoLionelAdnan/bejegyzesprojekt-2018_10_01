4e521a051fa361b06199e9b5325e30b4e4b375af
Petrik Lajos Szg <info@petrik.hu>
2018-10-01 13:12:53 +0200

    1. feladat

---
+++ BejegyzesProjekt/Bejegyzes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BejegyzesProjekt
+{
+    class Bejegyzes
+    {
+        private String szerzo;
+        private String tartalom;
+        private int likeok;
+        private DateTime letrejott;
+        private DateTime szerkesztve;
+
+        public Bejegyzes(string szerzo, string tartalom)
+        {
+            this.szerzo = szerzo;
+            this.tartalom = tartalom;
+            this.likeok = 0;
+            this.letrejott = ;
+        }
+    }
+
+
+}
